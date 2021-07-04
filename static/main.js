236550b220afd11ef78c118ee7a7c196ba6d8316
Snehal4315 <gamitsnehal107@gmail.com>
2021-07-04 12:03:57 +0530

    change

---
+++ static/main.js
@@ -0,0 +1,12 @@
+console.log('hii');
+var x;
+document.getElementById('error').style.color = 'red';
+document.getElementById('error').style.fontSize = '30px'
+
+x = document.getElementById('pre1').innerHTML;
+if(x == 'Placed'){
+    document.getElementById('pre1').style.color = 'green';
+}
+else{
+    document.getElementById('pre1').style.color = 'red'
+}
